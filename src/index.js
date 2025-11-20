73e90c198d3905913331c3189e76636ff344348a
matheussf <matheus@dzigual.com.br>
2025-11-20 19:16:10 -0300

    loaders webpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <h1 id="title">
-    <span>Oie Sophia</span>
+    <span>Oie Sophia!!</span>
   </h1>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-  entry: path.resolve(__dirname, "transpiled", "index.js"),
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle[hash].js",
@@ -13,4 +14,16 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  }
 };
