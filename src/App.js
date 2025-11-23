282429cfb2dcf0bbcb3e5ff645484d7538843753
matheussf <matheus@dzigual.com.br>
2025-11-23 19:55:50 -0300

    rederizando listas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Post from "./Post";
 import Header from "./Header";
+
+const posts = [
+  { id: 1, title: "Primeiro post", subtitle: "Meu primeiro post", likes: 30 },
+  { id: 2, title: "Segundo post", subtitle: "Meu segundo post", likes: 20 },
+  { id: 3, title: "Terceiro post", subtitle: "Meu terceiro post", likes: 50 },
+];
+
 function App() {
   return (
     <>
@@ -8,14 +15,14 @@ function App() {
         <h2>Posts da semana </h2>
       </Header>
       <hr />
-
-      <Post likes={20} title="matheus" subtitle="Subtitulo da noticia" />
-
-      <Post likes="20" title="Sophia" subtitle="Subtitulo da Sophia" />
-
-      <Post likes={30} title="Sophia" subtitle="Subtitulo da Matheus" />
-
-      <Post likes={230} title="matheus" subtitle="Subtitulo da noticia" />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          title={post.title}
+          subtitle={post.subtitle}
+          likes={post.likes}
+        />
+      ))}
     </>
   );
 }
