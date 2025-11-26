0e8b25472e51fa91505f488f23db090308db298c
matheussf <matheus@dzigual.com.br>
2025-11-25 21:16:40 -0300

    trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-  { id: 1, title: "Primeiro post", subtitle: "Meu primeiro post", likes: 30 },
-  { id: 2, title: "Segundo post", subtitle: "Meu segundo post", likes: 20 },
-  { id: 3, title: "Terceiro post", subtitle: "Meu terceiro post", likes: 50 },
-];
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Primeiro post", subtitle: "Meu primeiro post", likes: 30, read: false },
+    { id: 2, title: "Segundo post", subtitle: "Meu segundo post", likes: 20, read: true },
+    { id: 3, title: "Terceiro post", subtitle: "Meu terceiro post", likes: 50, read: true },
+  ]);
+  console.log(posts);
+
+  function handleRefresh() {
+    // posts.push();
+    // console.log("Posts atualizados:", posts);
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        title: `Post novo ${prevState.length + 1}`,
+        subtitle: `Meu post novo ${prevState.length + 1}`,
+        likes: Math.floor(Math.random() * 100),
+      },
+    ]);
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) => prevState.filter((post) => post.id !== postId));
+  }
+
   return (
     <>
       <Header title="Matheus">
-        <h2>Posts da semana </h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
       <hr />
       {posts.map((post) => (
         <Post
           key={post.id}
+          onRemove={handleRemovePost}
           title={post.title}
+          id={post.id}
+          read={post.read}
           subtitle={post.subtitle}
           likes={post.likes}
         />
--- src/Post.js
+++ src/Post.js
@@ -2,10 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Post(props) {
+if(props.read){
+  return <h2>{props.title} já foi lido</h2>
+}
+
+
   return (
     <>
       <article>
-        <strong>{props.title}</strong>
+        <strong>{props.read ? <s>{props.title}</s> : props.title}</strong>
+        <button onClick={() => props.onRemove(props.id)}>Remover</button>
         <br />
         <small>{props.subtitle}</small>
         <br />
@@ -18,8 +24,11 @@ function Post(props) {
 
 Post.propTypes = {
   likes: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
   title: PropTypes.string,
+  id: PropTypes.number.isRequired,
   subtitle: PropTypes.string,
+  read: PropTypes.bool.isRequired,
 };
 
 export default Post;
